59c4c68a176d252ae591563f95283e9e6a6c2889
bcardena <bryan.cardenas@udea.edu.co>
2019-08-04 18:11:23 -0500

    se adicionan nuevas funcionalidades como actualizar eliminar etc

---
+++ src/main/java/co/udea/heroes/api/model/Hero.java
@@ -0,0 +1,40 @@
+package co.udea.heroes.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "heroes")
+public class Hero {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nombre")
+    private String name;
+
+    public Hero(){
+
+    }
+
+    public Hero(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
